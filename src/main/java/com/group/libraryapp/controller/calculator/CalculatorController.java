23033978bd3fe746635e06d995fe47519c812864
미플 <devmeeple@gmail.com>
2024-04-27 16:19:59 +0900

    [Day 1] 1강 ~ 5강 학습
    - 서버 개발자 이론(Java, 프로젝트, 서버, 네트워크, HTTP & API)
    - GET API 개발하고 테스트하기(@GetMapping, @RequestParm)

---
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -0,0 +1,21 @@
+package com.group.libraryapp.controller.calculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.group.libraryapp.dto.request.CalculatorAddRequest;
+
+@RestController
+public class CalculatorController {
+
+	@GetMapping("/v1/add")
+	public int addTwoNumbersV1(@RequestParam int number1, @RequestParam int number2) {
+		return number1 + number2;
+	}
+
+	@GetMapping("/v2/add")
+	public int addTwoNumbersV2(CalculatorAddRequest request) {
+		return request.getNumber1() + request.getNumber2();
+	}
+}
