a5e5aa3ae5438861457f865c71f9bcff99bc695c
미플 <devmeeple@gmail.com>
2024-04-30 10:38:51 +0900

    [Day 3] 기본적인 데이터베이스 사용법
    - PostgreSQL, Docker Compose 추가

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,34 +1,41 @@
 package com.group.libraryapp.controller.user;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 
 @RestController
 public class UserController {
 
-	private final List<User> users = new ArrayList<>();
+	// TODO: 2024.04.30 13. 데이터베이스 사용
+	private final JdbcTemplate jdbcTemplate;
+
+	public UserController(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	@PostMapping("/user")
 	public void saveUser(@RequestBody UserCreateRequest request) {
-		users.add(new User(request.getName(), request.getAge()));
+		String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+		// TODO: 2024.04.30 데이터의 변경이 일어남을 나타냄 INSERT, UPDATE, DELETE 에 사용
+		jdbcTemplate.update(sql, request.getName(), request.getAge());
 	}
 
 	@GetMapping("/user")
 	public List<UserResponse> getUsers() {
-		List<UserResponse> responses = new ArrayList<>();
-		for (int i = 0; i < users.size(); i++) {
-			responses.add(new UserResponse(i + 1, users.get(i)));
-		}
-
-		return responses;
+		String sql = "SELECT * FROM user";
+		return jdbcTemplate.query(sql, (rs, rowNum) -> {
+			long id = rs.getLong("id");
+			String name = rs.getString("name");
+			int age = rs.getInt("age");
+			return new UserResponse(id, name, age);
+		});
 	}
 }
